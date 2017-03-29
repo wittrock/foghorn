accc5b94edf6f44d32da1a4d38722adbee4bf746
John Wittrock <john@sidewalklabs.com>
2017-03-29 17:57:15 -0400

    Working foghorn

--- foghorn.py
+++ foghorn.py
@@ -1,13 +1,18 @@
 import json
 import socket
+import subprocess
 import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 8000)
+server_address = ('localhost', 10110)
 print('starting on {}'.format(server_address))
 
 sock.bind(server_address)
+
+subprocess.Popen(['/home/jwittrock/src/rtl-ais/rtl_ais'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+print('Continuing...')
 
 while True:
     data, address = sock.recvfrom(4096)
